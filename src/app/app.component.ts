78edddf551c8fd8fa573440d98511cd36afc2973
Paulo Weskley de A. Ferreira <weskleyalmeida.si@gmail.com>
2021-01-02 20:15:19 -0300

    Criando o primeiro módulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = "My Pictures";
+  title = 'My Pictures';
+
+  photos = [
+    {
+      url:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
+      description: 'Leão',
+    },
+    {
+      url:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
+      description: 'Leoa',
+    },
+  ];
 }
