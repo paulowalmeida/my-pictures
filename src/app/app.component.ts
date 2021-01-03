5ea2a94286ef45dba271bc75d4d48988589d5d8c
Paulo Weskley de A. Ferreira <weskleyalmeida.si@gmail.com>
2021-01-02 21:44:12 -0300

    Criando a interface Photo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 import { PhotoService } from './photos/photo/photo.service';
+import { Photo } from './photos/photo/photo.model';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,7 @@ import { PhotoService } from './photos/photo/photo.service';
 })
 export class AppComponent {
   title = 'My Pictures';
-  photos$: Observable<any[]>;
+  photos$: Observable<Photo[]>;
 
   constructor(private photoService: PhotoService) {
     this.photos$ = this.photoService.getPhotos('flavio');
--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Photo } from './photo.model';
 
 @Injectable({ providedIn: 'root' })
 export class PhotoService {
@@ -9,6 +10,6 @@ export class PhotoService {
 
   getPhotos(userName: string): Observable<any[]> {
     const url = `${environment.URL_API}/${userName}/photos`;
-    return this.httpClient.get<any[]>(url);
+    return this.httpClient.get<Photo[]>(url);
   }
 }
