3b74aba100c4cb2d1c76c240a005b84aeb98275d
Paulo W. de A. Ferreira <weskleyalmeida.si@gmail.com>
2021-02-17 13:42:24 -0300

    Modularização dos components

--- src/app/photos/components/photo.components.module.ts
+++ src/app/photos/components/photo.components.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
-import { SharedModule } from 'src/app/shared/shared.module';
-
 import { PhotoListModule } from './photo-list/photo-list.module';
 import { PhotoLoadMoreModule } from './photo-load-more/photo-load-more.module';
 import { PhotoSearchModule } from './photo-search/photo-search.module';
+
 
 @NgModule({
   imports: [
